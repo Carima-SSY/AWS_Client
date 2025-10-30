f8f4e475462d018ee0d44ba19203f7014de2b551
Carima-SSY <sysim@carima.co.kr>
2025-10-30 12:39:38 +0900

    Edit 1030

--- lib/file_manager.py
+++ lib/file_manager.py
@@ -142,8 +142,9 @@ class FileManager:
         
         print_data = dict() 
         for slice in slices:
-            split_str = str(slice).split('/')
-            name = split_str[len(split_str)-1]
+            # split_str = str(slice).split('/')
+            # name = split_str[len(split_str)-1]
+            name = os.path.basename(slice)
             files = self.get_files(slice)
             preview = self.get_previewimg(files)
             encoded = self.encode_previewimg(preview[0], 120)
@@ -160,8 +161,12 @@ class FileManager:
             recipe_dic = dict()
             for file in files:
                 if self.is_recipefile(file):
-                    recipe_dic[file.split('/')[len(file.split('/'))-1]] = {
-                        # "content": self.encode_recipe(file),
+                    # recipe_dic[file.split('/')[len(file.split('/'))-1]] = {
+                    #     # "content": self.encode_recipe(file),
+                    #     "content": self.convert_xml_to_json(file),
+                    #     "size": os.path.getsize(file)
+                    # }
+                    recipe_dic[os.path.basename(file)] = {
                         "content": self.convert_xml_to_json(file),
                         "size": os.path.getsize(file)
                     }
